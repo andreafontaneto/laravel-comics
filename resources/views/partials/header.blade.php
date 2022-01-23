b5ca3eae3c6ea6236ee745b52df0f0f87519c0eb
andreafontaneto <andrea.fontaneto@gmail.com>
2022-01-23 16:56:34 +0100

    init project w/ Stefano

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+  <nav>
+    <ul>
+      <li><a href="{{ route('home') }}">HOME</a></li>
+      <li><a href="{{ route('about') }}">ABOUT</a></li>
+      <li><a href="{{ route('contacts') }}">CONTACT</a></li>
+    </ul>
+  </nav>
+</header>
